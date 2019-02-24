6412de4d3b6c15c484e846c11ab6ce14a0137e9a
Jefferson V. Henry <jeffvhenry@gmail.com>
2019-02-24 17:48:18 -0600

    Finished week1
    No programming problems for week1

---
+++ Week1/main.cpp
@@ -0,0 +1,13 @@
+// Jefferson V. Henry 
+// CIS-123 
+// Week 2, Program 1
+// ~~ Instructions ~~
+// No programs for week1
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // oof
+    return 0;
+}
