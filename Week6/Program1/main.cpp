cb16f897453119098c6fa1b94bfbf5add0b67e14
Jefferson V. Henry <jeffvhenry@gmail.com>
2019-04-20 19:37:24 -0500

    Started week6

---
+++ Week6/Program1/main.cpp
@@ -0,0 +1,29 @@
+// Jefferson V. Henry 
+// CIS-123 
+// Week 6, Program 1
+// ~~ Instructions ~~
+// Write a program that asks the user to input an integer number. Then starting
+// from that number the program prints out in the console next 100 numbers.
+// eg.
+// User inputted:
+// 11
+// Output:
+// 12
+// 13
+// 14
+// 15
+// ...
+// 111
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+    for (int i = 0; i < 100; ++i) {
+        cout << ++num << endl;
+    }
+    // system("pause");
+    return 0;
+}
---
+++ Week6/Program2/main.cpp
@@ -0,0 +1,33 @@
+// Jefferson V. Henry 
+// CIS-123 
+// Week 6, Program 1
+// ~~ Instructions ~~
+// Write a program that asks the user to input any number (integer or floating
+// point) and the n-th power (integer) the number should be raised to. Then
+// calculate the result of the exponentiation of this number to the said power.
+// eg.
+// User inputted:
+// Number: 2
+// N-th power: 5
+// Output:
+// The number 2 to the power of 5 is equal to 32.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, pow, result;
+    cout << "Number: ";
+    cin >> num;
+    cout << "Power: ";
+    cin >> pow;
+    result = num;
+
+    for (int i = 0; i < pow; ++i) {
+        result *= num;
+    }
+    cout << "Result: " << result << endl;
+
+    system("pause");
+    return 0;
+}
